f9966c5b8982bd61084098ce13464689d38b8c83
hasan-kayan <hasankayan2000@hotmail.com>
2023-11-27 15:17:35 +0300

    State

---
+++ react-tf-cad/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count] = useState(0)
+  console.log(count)
+  return (
+   
+<div>
+
+</div>
+
+  )
+}
+
+export default App
